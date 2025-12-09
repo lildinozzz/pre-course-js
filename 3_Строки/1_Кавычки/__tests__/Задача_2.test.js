bbc8758339a5340028ae9ca1905c8d7cddc38dbb
Artur Kim <a.al.kim@uzum.com>
2025-12-09 13:03:18 +0300

    FIX: fix test

--- 3_Строки/1_Кавычки/__tests__/Задача_2.test.js
+++ 3_Строки/1_Кавычки/__tests__/Задача_2.test.js
@@ -1,16 +1,16 @@
 describe("Task 2: Print string with quotes and its length", () => {
   let consoleLogSpy;
 
-  beforeEach(() => {
+  beforeAll(() => {
     consoleLogSpy = jest.spyOn(console, "log").mockImplementation();
+    require("../Задача_2.js");
   });
 
-  afterEach(() => {
+  afterAll(() => {
     consoleLogSpy.mockRestore();
   });
 
   test('should print "Кот сказал: "Мяу!""', () => {
-    require("../Задача_2.js");
     expect(consoleLogSpy).toHaveBeenNthCalledWith(1, 'Кот сказал: "Мяу!"');
   });
 
