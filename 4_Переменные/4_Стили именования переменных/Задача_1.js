eb2fbe31ec7f6d00213ff41acf1bb1fbf17590aa
Artur Kim <a.al.kim@uzum.com>
2025-12-10 18:20:19 +0300

    FIX: ADD INFO

--- 4_Переменные/4_Стили именования переменных/Задача_1.js
+++ 4_Переменные/4_Стили именования переменных/Задача_1.js
@@ -1,1 +1,20 @@
-// Пишите код тут
+const first_name = "John"; // camelCase
+console.log("firstName");
+
+const last_name = "Doe"; //
+// напиши console.log с названием переменной в стиле camelCase
+
+const age = 30;
+// напиши console.log с названием переменной в стиле UPPER_SNAKE_CASE
+
+const MobilePhone = "+7 999 999 99 99";
+// напиши console.log с названием переменной в стиле UPPER_SNAKE_CASE
+
+const USER_EMAIL = "john@doe.com";
+// напиши console.log с названием переменной в стиле snake_case
+
+const date_of_birth = "01.01.1970";
+// напиши console.log с названием переменной в стиле PascalCase
+
+const address = "Moscow, Russia";
+// напиши console.log с названием переменной в стиле PascalCase
